e87efe821f73ed6bb8dc81fa2120312678b9befc
woohiq <tkdqls92@gmail.com>
2023-01-16 23:42:50 +0900

    0116 update

---
+++ han/0116/q2193.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class q2193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+        long[] ans = new long[N+1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for (int i = 2; i<=N; i++){
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        System.out.println(ans[N]);
+    }
+}
