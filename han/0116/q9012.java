a40f1e7cf676f896d65d7e64ef506bb7e02618b1
woohiq <tkdqls92@gmail.com>
2023-01-16 17:22:20 +0900

    0116 update

--- han/0116/q9012.java
+++ han/0116/q9012.java
@@ -1,18 +1,19 @@
 import java.io.*;
-class q9012{
-    public static void main(String[] args) throws IOException{
+public class q9012 {
+	public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        here : for (int i = 0; i<N; i++){
+        for (int i = 0; i<N; i++){
             String input = br.readLine();
             int okay = 0;
+            boolean valid = true;
             for (int j =0; j<input.length(); j++){
                 if (input.charAt(j) == '(') okay++;
                 else okay--;
-                if (okay <= 0) {System.out.println("NO"); break here;}
+                if (okay < 0) valid = false;
             }
-            System.out.println("YES");
+            if (valid == true && okay == 0) System.out.println("YES");
+            else System.out.println("NO");
         }
-
     }
 }
